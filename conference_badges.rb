73b55022a9c0bb48c509e6fddded17c927ffd9aa
Austin Asbury <acasbury25@aol.com>
2019-08-17 22:40:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker (name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator (list)
+  list_of_badges = []
+  list.each do |name|
+    list_of_badges.push(badge_maker(name))
+  end
+  list_of_badges
+end
